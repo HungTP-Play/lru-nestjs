0053ba07715655cf4211c51fe2f7b904e8e07499
Hung Tran <hung.p.tran@opswat.com>
2023-07-12 09:03:40 +0700

    chore: change PORT

--- analytic/src/main.ts
+++ analytic/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 4444;
+  await app.listen(PORT);
 }
 bootstrap();
